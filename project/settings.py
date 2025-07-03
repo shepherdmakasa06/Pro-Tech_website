71f9c8327e2b9961a0447e3ae9624fcb8b3617bd
shepherdmakasa06 <shepherdmakasa06@gmail.com>
2025-07-03 14:13:05 +0200

    Update settings.py for production deployment

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nvn-mz#-lqd$)-=u4j$$=7@@w$=asbl=xi0v@xsksdoej7x324'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://pro-tech-website-1.onrender.com']
 
 
 # Application definition
