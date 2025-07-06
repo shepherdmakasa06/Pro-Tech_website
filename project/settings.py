580657858f618d67e569762f6a4dc4bb8309f168
shepherdmakasa06 <shepherdmakasa06@gmail.com>
2025-07-06 16:35:46 +0200

    Enable debug mode for static file testing

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nvn-mz#-lqd$)-=u4j$$=7@@w$=asbl=xi0v@xsksdoej7x324'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['pro-tech-website-1.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','pro-tech-website-1.onrender.com']
 
 
 # Application definition
@@ -116,9 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
