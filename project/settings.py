42d337e611a8c1806b6e86ccf41c65ba1642ab85
shepherdmakasa06 <shepherdmakasa06@gmail.com>
2025-07-13 22:06:27 +0200

    upload_1

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nvn-mz#-lqd$)-=u4j$$=7@@w$=asbl=xi0v@xsksdoej7x324'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False 
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','pro-tech-website-1.onrender.com']
